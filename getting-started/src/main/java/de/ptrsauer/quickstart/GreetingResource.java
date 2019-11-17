12fe6cbe84ffe7e77b5b4dc01c5ff86f67f8e9aa
Peter Sauer <peter.sauer@europace.de>
2019-11-17 20:10:27 +0100

    Native image creation & async GreetingResource

--- getting-started/src/main/java/de/ptrsauer/quickstart/GreetingResource.java
+++ getting-started/src/main/java/de/ptrsauer/quickstart/GreetingResource.java
@@ -1,10 +1,9 @@
 package de.ptrsauer.quickstart;
 
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.concurrent.CompletableFuture;
@@ -19,19 +18,12 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    public String greeting(@PathParam String name) {
-        return service.greeting(name);
+    public CompletionStage<String> greeting(@PathParam("name") String name) {
+        return CompletableFuture.supplyAsync(() -> service.greeting(name));
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/async")
     public CompletionStage<String> helloAsync() {
         return CompletableFuture.supplyAsync(() -> "hello");
     }
